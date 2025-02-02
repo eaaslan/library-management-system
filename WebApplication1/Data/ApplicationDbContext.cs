8addfed08e029c53473a2ec9ee3927d3d4e00942
eaaslan <ensa.aslan@gmail.com>
2025-02-02 12:40:48 +0300

    Changed DbContex to IdentityDbContext

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -1,15 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
 
 namespace WebApplication1.Data
 {
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext :IdentityDbContext<AppUser,AppRole,string>
 
     {
 
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { } // send options(connectionstring via Program.cs) to the base class's constructor
 
-       public DbSet<Book>  Books { get; set; }
+        public DbSet<Book>  Books { get; set; }
 
     }
 }
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Abstracts;
 using WebApplication1.Data;
+using WebApplication1.Models;
 using WebApplication1.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,8 @@ builder.Services.AddScoped<IBookService, BookService>();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
 // we have to tell that which class has the implementation of db context
 options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))); // to reach apsettings.json for connectionString use builder.Configuration
+
+builder.Services.AddIdentity<AppUser,AppRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
